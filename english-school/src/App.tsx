f38d67810e93d64a9d34c26041928c0564ab1282
Yana <hatskoyana@gmail.com>
2026-02-24 20:18:56 +0100

    feat(app): update routing and layout structure

--- english-school/src/App.tsx
+++ english-school/src/App.tsx
@@ -1,7 +1,29 @@
-import AppRouter from "./router/AppRouter";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header.tsx";
+import Footer from "./components/Footer/Footer.tsx";
+
+import About from "./pages/About/About";
+import Courses from "./pages/Courses/Courses";
+import FAQ from "./pages/FAQ/FAQ";
+import Apply from "./pages/Apply/Apply";
+import Contacts from "./pages/Contacts/Contacts";
+import News from "./pages/News/News";
 
 function App() {
-  return <AppRouter />;
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<About />} />
+        <Route path="/courses" element={<Courses />} />
+        <Route path="/faq" element={<FAQ />} />
+        <Route path="/apply" element={<Apply />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/news" element={<News />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
